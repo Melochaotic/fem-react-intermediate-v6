da6728863b636e4f4730945dfa02158dbbb4a366
Nicko J. Ruddock <nicholas.ruddock@taxassist.co.uk>
2025-10-20 14:13:37 +0100

    🎉 RSC from scratch setup

---
+++ starter/no-framework/babel.config.js
@@ -0,0 +1,12 @@
+const development = (process.env.NODE_ENV || "development") === "development";
+
+module.exports = {
+  presets: [
+    "@babel/preset-react",
+    {
+      runtime: "automatic",
+      useSpread: true,
+      development,
+    },
+  ],
+};
