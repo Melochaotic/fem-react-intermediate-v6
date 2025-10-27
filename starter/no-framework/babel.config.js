737999cf2a7cb18b348979b6c13e06abd797ce76
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-10-27 13:38:41 +0000

    ✏️ fix babel typo

--- starter/no-framework/babel.config.js
+++ starter/no-framework/babel.config.js
@@ -2,11 +2,13 @@ const development = (process.env.NODE_ENV || "development") === "development";
 
 module.exports = {
   presets: [
-    "@babel/preset-react",
-    {
-      runtime: "automatic",
-      useSpread: true,
-      development,
-    },
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+        useSpread: true,
+        development: true,
+      },
+    ],
   ],
 };
